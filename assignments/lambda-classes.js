eb6f12f0893fa638187f8d52b3f9c07a66937cec
rachell2g9 <rachellsincere@gmail.com>
2019-05-09 23:04:33 -0400

    completed student class and 1 example

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,7 +13,7 @@ class Person {
     }
 }
 
-//Person class example
+//Person class examples
 const sammi = new Person ({
     name: 'Sammi',
     age: '23',
@@ -21,6 +21,8 @@ const sammi = new Person ({
     gender: 'F',
 })
 console.log(sammi.speak());
+console.log(sammi.gender);
+
 
 
 
@@ -43,13 +45,53 @@ class Instructor extends Person {
 
 //Instructor Class example
 const lori = new Instructor({
-    name: 'Sammi',
-    age: '23',
-    location: 'Santa Monica',
+    name: 'Lori',
+    age: '35',
+    location: 'Anchorage',
     gender: 'F',
-    specialty: 'Front-end',
+    specialty: 'Front-End',
     favLanguage: 'Javascript',
     catchPhrase: 'Cash rules everything around me'
 })
+console.log(lori.catchPhrase);
 console.log(lori.demo("Applied Js"));
 console.log(lori.grade(sammi, "Calculus"));
+
+
+class Student extends Person {
+    constructor(stuAttr) {
+        super(stuAttr);
+        this.previousBackground = stuAttr.previousBackground;
+        this.className = stuAttr.className;
+        this.favSubjects = stuAttr.favSubjects;
+    }
+    listsSubjects() {
+        this.favSubjects.forEach(element => {
+            console.log(element); //console.logged it instead of return so it would list them.
+          })
+    }
+    prAssignment(prSubject) {
+        return `${this.name} has submitted a PR for ${prSubject}`;
+    }
+    sprintChallenge(sprintSubject) {
+        return `${this.name} has begun sprint challenge on ${sprintSubject}`;
+    }
+}//Student Class
+
+//Student Class Example
+const jeremy = new Student({
+    name: 'Jeremy',
+    age: '63',
+    location: 'Florida',
+    gender: 'M',
+    previousBackground: 'accountant',
+    className: 'CS3401',
+    favSubjects: ['math', 'art', 'writing', 'music']
+})
+
+jeremy.listsSubjects();
+console.log(jeremy.age);
+console.log(jeremy.prAssignment('javascript2'));
+console.log(jeremy.sprintChallenge('javascript4'));
+
+
